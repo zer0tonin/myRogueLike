9d1c57b8b804a6d936cc32762310acd6bbf7d22d
zer0tonin <bouhahah@gmail.com>
2017-10-18 10:52:51 +0200

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ use tcod::colors::{self, Color};
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const LIMIT_FPS: i32 = 20;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+
 fn main() {
-    const SCREEN_WIDTH: i32 = 80;
-    const SCREEN_HEIGHT: i32 = 50;
-    const LIMIT_FPS: i32 = 20;
 
     let mut root = Root::initializer()
             .font("arial10x10.png", FontLayout::Tcod)
@@ -17,7 +22,7 @@ fn main() {
             .title("Rust/libtcod tutorial")
             .init();
 
-    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     tcod::system::set_fps(LIMIT_FPS);
     
@@ -25,11 +30,11 @@ fn main() {
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::WHITE);
     let mut objects = [player, npc];
 
+    let mut map = make_map();
+
     while !root.window_closed() {
-        for object in &objects {
-            object.draw(&mut con);
-        }
-        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        render_all(&mut root, &mut con, &mut objects, &map);
+        blit(&mut con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
         root.flush();
 
         for object in &objects {
@@ -37,7 +42,7 @@ fn main() {
         }
 
         let player = &mut objects[0];
-        let exit = handle_keys(&mut root, player);
+        let exit = handle_keys(&mut root, player, &map);
         if exit {
             break;
         }
@@ -46,7 +51,7 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     let key = root.wait_for_keypress(true);
     match key {
         Key { code: Enter, alt: true, .. } => {
@@ -56,10 +61,10 @@ fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
         },
         Key { code: Escape, .. } => return true,
 
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, map),
+        Key { code: Down, .. } => player.move_by(0, 1, map),
+        Key { code: Left, .. } => player.move_by(-1, 0, map),
+        Key { code: Right, .. } => player.move_by(1, 0, map),
 
         _ => {},
     }
@@ -83,9 +88,13 @@ impl Object {
         }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32) {
-        self.x += dx;
-        self.y += dy;
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !(self.x + dx >= MAP_WIDTH) && !(self.y + dy >= MAP_HEIGHT) {
+            if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+                self.x += dx;
+                self.y += dy;
+            } 
+        }
     }
 
     pub fn draw(&self, con: &mut Console) {
@@ -95,5 +104,48 @@ impl Object {
 
     pub fn clear(&self, con: &mut Console) {
         con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, block_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, block_sight: true }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for object in objects {
+        object.draw(con);
+    }
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].block_sight;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
     }
 }
